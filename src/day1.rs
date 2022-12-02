bdc3e7eb4d150ffa58bab68f9faef74797560060
Adam Kramer <akramer@gmail.com>
2022-12-01 18:24:20 -0800

    forgot to format

--- src/day1.rs
+++ src/day1.rs
@@ -22,12 +22,12 @@ pub fn solve_part2(input: &str) -> i32 {
     let mut current_sum = 0;
     for l in input.lines() {
         if l == "" {
-          for i in max.iter_mut() {
-            if current_sum > *i {
-              (*i, current_sum) = (current_sum, *i);
+            for i in max.iter_mut() {
+                if current_sum > *i {
+                    (*i, current_sum) = (current_sum, *i);
+                }
             }
-          }
-          current_sum = 0;
+            current_sum = 0;
         } else {
             let c: i32 = l.parse().unwrap();
             current_sum += c;

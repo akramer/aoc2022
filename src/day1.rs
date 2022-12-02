9e83aa7e4dac2eb15d0c3c5e0140766ed1965225
Adam Kramer <akramer@gmail.com>
2022-12-01 18:22:12 -0800

    Checking in day1

---
+++ src/day1.rs
@@ -0,0 +1,37 @@
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &str) -> i32 {
+    let mut max = 0;
+    let mut current_sum = 0;
+    for l in input.lines() {
+        if l == "" {
+            if current_sum > max {
+                max = current_sum;
+            }
+            current_sum = 0;
+        } else {
+            let c: i32 = l.parse().unwrap();
+            current_sum += c;
+        }
+    }
+    return max;
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &str) -> i32 {
+    let mut max = vec![0; 3];
+    let mut current_sum = 0;
+    for l in input.lines() {
+        if l == "" {
+          for i in max.iter_mut() {
+            if current_sum > *i {
+              (*i, current_sum) = (current_sum, *i);
+            }
+          }
+          current_sum = 0;
+        } else {
+            let c: i32 = l.parse().unwrap();
+            current_sum += c;
+        }
+    }
+    return max.iter().sum();
+}
